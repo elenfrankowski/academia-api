d276a9d67cfb12ac352275f038de1c454399ce7a
Elen Frankowski <frankowski.elen@gmail.com>
2026-08-24 14:08:53 -0300

    refactor: migra modulo instrutores para NestJS

--- src/instrutores/usecases/criar-instrutor.uc.ts
+++ src/instrutores/usecases/criar-instrutor.uc.ts
@@ -1,10 +1,13 @@
+import { Inject, Injectable } from '@nestjs/common'
+
 import { ehTextoValido } from '../../@common/platform/utils/validator.utils'
 import { Instrutor } from '../../@common/entities/instrutor.entity'
 import { CriarInstrutorDto } from '../dtos/criar-instrutor.dto'
 import { InstrutorRepository } from '../repositories/instrutor.repository'
 
+@Injectable()
 export class CriarInstrutorUc {
-  constructor(private readonly repository: InstrutorRepository) {}
+  constructor(@Inject('InstrutorRepository') private readonly repository: InstrutorRepository) {}
 
   async executar(dto: CriarInstrutorDto): Promise<Instrutor> {
     if (!ehTextoValido(dto.nome) || !ehTextoValido(dto.especialidade) || !ehTextoValido(dto.registro)) {
